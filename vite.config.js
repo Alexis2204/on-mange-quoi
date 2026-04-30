cac2d22be2165bddb305cdfb8e43d2118f183f29
Alexis <alex.richard220499@gmail.com>
2026-04-30 23:23:44 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,37 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'On Mange Quoi',
+        short_name: 'OMQ',
+        description: 'Choisir quoi manger facilement',
+        theme_color: '#7C3AED',
+        background_color: '#F8F5F2',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
